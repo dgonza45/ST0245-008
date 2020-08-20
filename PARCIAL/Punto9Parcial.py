9cdfec7dbffcd40df1cf5e6bf866ffbc1905843f
dgonza45 <69641287+dgonza45@users.noreply.github.com>
2020-08-20 11:44:07 -0500

    Add files via upload

---
+++ PARCIAL/Punto9Parcial.py
@@ -0,0 +1,26 @@
+x = int(input("Ingrese fila a calcular "))
+y = int(input("Ingrese columna a calcular "))
+
+
+def pascal(row, column):
+    if row < 0 and column < 0:
+        return 0
+    elif row == 0 and column == 0:
+        return 1
+    else:
+        print("En la posicion", row, column, "esta el numero", combinacion(row-1, column-1))
+
+
+def combinacion(n, k):
+    return int((factorial(n)) / (factorial(k) * (factorial(n - k))))
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+pascal(x, y)
+
